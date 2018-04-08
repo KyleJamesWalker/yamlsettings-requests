c92d7f44d4e2b8ffd5a942637081795ff11d0f77
Kyle Walker <Kyle.Walker@zefr.com>
2018-04-08 14:48:34 -0700

    Lower version to test deployment

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements.update(all=sorted(set().union(*requirements.values())))
 
 setup(
     name='yamlsettings-requests',
-    version='1.0.0',
+    version='0.0.1',
     author='Kyle Walker',
     author_email='KyleJamesWalker@gmail.com',
     description='YamlSettings Request Extension',
